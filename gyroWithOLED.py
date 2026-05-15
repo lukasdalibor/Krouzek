d9006d6f8f4c3d9ba0b94c6e8d6f26348dbf95f7
lukasdalibor <94167569+lukasdalibor@users.noreply.github.com>
2026-05-15 15:51:28 +0200

    Add files via upload

---
+++ gyroWithOLED.py
@@ -0,0 +1,88 @@
+from machine import Pin, I2C
+import utime
+import MPU6050
+import math
+import time
+import ssd1306
+
+
+i2coled = I2C(1, scl=Pin(19), sda=Pin(18))
+oled = ssd1306.SSD1306_I2C(128, 64, i2coled)
+
+
+# yellow: SCL on GP17 (pin 22), blue: SDA on GP16 (pin 21)
+i2c = I2C(0, scl = Pin(17), sda = Pin(16), freq = 100000)
+mpu = MPU6050.MPU6050(i2c)
+mpu.wake()
+
+dt = 10
+alpha = 0.96
+
+avgAccel = [0.0,0.0,0.0]
+for i in range(2000):
+    accel = mpu.read_accel_data()
+    avgAccel[0] += accel[0]/2000
+    avgAccel[1] += accel[1]/2000
+    avgAccel[2] += accel[2]/2000
+accelX = avgAccel[0] / math.sqrt(accel[0]*accel[0]+accel[1]*accel[1]+accel[2]*accel[2])
+accelY = avgAccel[1]
+accelZ = avgAccel[2]
+lastPhiDeg = math.atan(accelY/accelZ)*180.0/math.pi
+lastThetaDeg = math.asin(accelX)*180.0/math.pi
+
+lastt = time.ticks_ms()
+utime.sleep_ms(dt)
+
+while True:
+    # data acquisition: unit gravity vector and body-frame rates [deg/s]
+    accel = mpu.read_accel_data()
+    gyro = mpu.read_gyro_data()
+    t = time.ticks_ms()
+    
+    # accelerometer data processing: Euler angles
+    accelX = accel[0] / math.sqrt(accel[0]*accel[0]+accel[1]*accel[1]+accel[2]*accel[2])
+    accelY = accel[1]
+    accelZ = accel[2]
+    phi = math.atan(accelY/accelZ)
+    phiDeg = phi*180.0/math.pi
+    theta = math.asin(accelX)
+    thetaDeg = theta*180.0/math.pi
+    
+    # gyroscope data processing: body-frame rates to Euler rates
+    p = gyro[0]
+    q = -gyro[1]
+    r = -gyro[2]
+    sphi = math.sin(phi)
+    cphi = math.cos(phi)
+    ttheta = math.tan(theta)
+    phiDegDot = p + ttheta*(sphi*q + cphi*r)
+    thetaDegDot = cphi*q-sphi*r
+
+    # sensor fusion: complementary filter
+    phiDeg = (1.0-alpha)*phiDeg + alpha*(lastPhiDeg+phiDegDot*(t-lastt)/1000)
+    thetaDeg = (1.0-alpha)*thetaDeg + alpha*(lastThetaDeg+thetaDegDot*(t-lastt)/1000)
+    lastPhiDeg = phiDeg
+    lastThetaDeg = thetaDeg
+    print(phiDeg,thetaDeg)
+    
+    sp = math.sin(phi)
+    cp = math.cos(phi)
+    st = math.sin(theta)
+    ct = math.cos(theta)
+    i = [ct,0.0,-st]
+    j = [sp*st,cp,sp*ct]
+    k = [cp*st,-sp,cp*ct]
+    ix = (i[0]-i[1])/math.sqrt(2.0)
+    iy = (-i[0]-i[1]+2*i[2])/math.sqrt(6.0)
+    jx = (j[0]-j[1])/math.sqrt(2.0)
+    jy = (-j[0]-j[1]+2*j[2])/math.sqrt(6.0)
+    kx = (k[0]-k[1])/math.sqrt(2.0)
+    ky = (-k[0]-k[1]+2*k[2])/math.sqrt(6.0)
+    oled.fill(0)
+    oled.draw_line(64,32,math.floor(64+32*ix),math.floor(32+32*iy),1)
+    oled.draw_line(64,32,math.floor(64+32*jx),math.floor(32+32*jy),1)
+    oled.draw_line(64,32,math.floor(64+32*kx),math.floor(32+32*ky),1)
+    oled.show()
+    
+    lastt = t
+    utime.sleep_ms(dt)
